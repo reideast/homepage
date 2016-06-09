b63da710c3fc30dad749db97240e6b1528f8057b
Andrew Reid East <andrew@andreweast.net>
2016-06-09 11:37:40 +0100

    Convert to single size of top & base of headerBG

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 $(document).ready(function () {
-    var colors = ["#EBBD8A", "#C5A27D", "#BD915D"];
-    var currColor = 0;
-    function chgColor() {
-        $("body").css("background-color", colors[(currColor = (currColor + 1) % 3)]);
-        console.log(currColor + " " + colors[currColor]);
-        setTimeout(chgColor, 2000);
-    }
+    // var colors = ["#EBBD8A", "#C5A27D", "#BD915D"];
+    // var currColor = 0;
+    // function chgColor() {
+    //     $("body").css("background-color", colors[(currColor = (currColor + 1) % 3)]);
+    //     console.log(currColor + " " + colors[currColor]);
+    //     setTimeout(chgColor, 2000);
+    // }
     //chgColor();
+
+    $(document).scroll(function () {
+        // var top = $(window).scrollTop();
+        // var height = $(window).height();
+        // var percentScrolled = top / height;
+        // console.log(percentScrolled + "%");
+        // var opacity = 1 - percentScrolled;
+        // console.log(opacity);
+        $("#headerBaseImage").css("opacity", Math.max(0, 1 - ($(window).scrollTop() / $(window).height())) + ""); // opacity = 1 - percentScrolled
+        if ($(window).scrollTop() > $(window).height() + 144) {
+            console.log("below");
+        } else {
+            console.log("above");
+        }
+    });
 });
