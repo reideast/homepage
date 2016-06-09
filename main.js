f67a32f1af99acb3c86622a818e12d01bc022e78
Andrew Reid East <andrew@andreweast.net>
2016-06-09 21:00:53 +0100

    Implement animation to reveal roots

--- main.js
+++ main.js
@@ -9,14 +9,27 @@ $(document).ready(function () {
     //chgColor();
 
     $(document).scroll(function () {
-        // var top = $(window).scrollTop();
+        var top = $(window).scrollTop();
+        console.log("scrollTop=" + top);
         // var height = $(window).height();
         // var percentScrolled = top / height;
         // console.log(percentScrolled + "%");
         // var opacity = 1 - percentScrolled;
         // console.log(opacity);
-        $("#headerBaseImage").css("opacity", Math.max(0, 1 - ($(window).scrollTop() / $(window).height())) + ""); // opacity = 1 - percentScrolled
-        if ($(window).scrollTop() > $(window).height() + 144) {
+        // $("#headerBaseImage").css("opacity", Math.max(0, 1 - ($(window).scrollTop() / $(window).height())) + ""); // opacity = 1 - percentScrolled
+        
+        
+        if (top > 80) {
+            console.log("fadeOut");
+            $("#headerBaseImage").fadeOut(1000);
+            $("#rootsImageSoil").fadeOut(1000);
+        } else {
+            console.log("fadeIn");
+            $("#headerBaseImage").fadeIn(200);
+            $("#rootsImageSoil").fadeIn(200);
+        }
+
+        if (top > $(window).height() + 144) {
             console.log("below");
         } else {
             console.log("above");
