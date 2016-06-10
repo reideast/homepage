83eea0e0be725bcabe80709bbc9296cd2a988d57
Andrew Reid East <andrew@andreweast.net>
2016-06-10 12:53:18 +0100

    Create About text boxes with fade effect

--- main.js
+++ main.js
@@ -8,10 +8,15 @@ $(document).ready(function () {
     // }
     //chgColor();
 
+    $("#headerBaseImage").show();
+    $("#rootsImageSoil").show();
+    $("#about1, #about2, #about3").hide();
+
     $(document).scroll(function () {
         var top = $(window).scrollTop();
         console.log("scrollTop=" + top);
-        // var height = $(window).height();
+        var height = $(window).height();
+        // console.log("height=" + height);
         // var percentScrolled = top / height;
         // console.log(percentScrolled + "%");
         // var opacity = 1 - percentScrolled;
@@ -20,16 +25,27 @@ $(document).ready(function () {
         
         
         if (top > 80) {
-            console.log("fadeOut");
+            // console.log("fadeOut");
             $("#headerBaseImage").fadeOut(1000);
             $("#rootsImageSoil").fadeOut(1000);
         } else {
-            console.log("fadeIn");
+            // console.log("fadeIn");
             $("#headerBaseImage").fadeIn(200);
             $("#rootsImageSoil").fadeIn(200);
+            $("#about1, #about2, #about3").hide();
         }
 
-        if (top > $(window).height() + 144) {
+        if (top > 400) {
+            $("#about1").slideDown(1000);
+        }
+        if (top > 550) {
+            $("#about2").slideDown(1000);
+       }
+        if (top > 700) {
+            $("#about3").slideDown(1000);
+        } 
+    
+        if (top > height) {
             console.log("below");
         } else {
             console.log("above");
