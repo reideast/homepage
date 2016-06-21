bca85740b26f0d34d660f91579cf299c7b144658
Andrew Reid East <andrew@andreweast.net>
2016-06-21 22:25:33 +0100

    Have Roots Wireframe appear

--- main.js
+++ main.js
@@ -87,6 +87,7 @@ $(document).ready(function () {
     function showRoots() {
         $("#rootsOverlay").fadeOut(1000, function () {
             console.log("rootsOverlay fadeOut callback");
+            vivusWireframe.play();
             vivusRoots.forEach(function (item, i) {
                 console.log("foreach vivusRoots[" + i + "]");
                 item.play();
@@ -95,6 +96,7 @@ $(document).ready(function () {
     }
     function hideRoots() {
         $("#rootsOverlay").fadeIn(700);
+        vivusWireframe.reset();
         vivusRoots.forEach(function (item) {
             item.reset();
         });
@@ -108,7 +110,7 @@ $(document).ready(function () {
     // var vivusRoots2 = new Vivus('rootIntro2', {duration: 70, type: "async"}, function () {console.log("Vivus on rootIntro2 is done"); });
     // var vivusRoots3 = new Vivus('rootIntro3', {duration: 70, type: "async"}, function () {console.log("Vivus on rootIntro3 is done"); });
 
-    // var vivusWireframe = new Vivus('roots')
+    var vivusWireframe = new Vivus('rootsWireframe', {start: "manual", duration: 75, delay: 30, type: "oneByOne"});
 
     $('#fullpage').fullpage({
         // modified my copy of Fullscreen.js to not require this: scrollBar: true // scrollBar==true (or autoScrolling==false) must be set to allow other onScroll events to fire! (because w/o this option, the window doesn't actually scroll, as shown by observing $(window).scrollTop())
